a181663928be0b7d64121042b1d5f108ef59f632
bocah96 <146477040+bocah96@users.noreply.github.com>
2023-09-30 00:51:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var tombolMenu = document.getElementsByClassName('tombol-menu')[0];
+var menu = document.getElementsByClassName('menu')[0];
+
+tombolMenu.onclick = function() {
+    menu.classList.toggle('active');
+}
+
+menu.onclick = function() {
+    menu.classList.toggle('active');
+}
+document.getElementById('aboutLink').addEventListener('click', function() {
+    window.location.href = 'tim.html';
+  });
+  document.getElementById('aboutLink').addEventListener('click', function() {
+    window.location.href = 'index.html';
+  });
+  document.getElementById('aboutLink').addEventListener('click', function() {
+    window.location.href = 'tentang.html';
+  });
